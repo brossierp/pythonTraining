62878097d743b0bb9b47bef8861f3794e4cd1ab5
Philippe Brossier <Philippe.Brossier@ordnancesurvey.co.uk>
2015-09-09 11:20:28 +0100

    now importing the re module

--- out/production/pythonTraining/simpleprograms/printing.py
+++ out/production/pythonTraining/simpleprograms/printing.py
@@ -1,5 +1,10 @@
 __author__ = 'PBrossier'
 
+
+# Import module regular expression
+import re
+
+
 # Code from https://wiki.python.org/moin/SimplePrograms
 print 'Hello, world!\n'
 
@@ -15,3 +20,27 @@ print 'With format method -- Hi, {} \n'.format(inputName)
 friends = ['john', 'pat', 'gary', 'michael']
 for i, name in enumerate(friends):
     print "iteration {iterationPlaceHolder} is {namePlaceHolder}".format(iterationPlaceHolder=i, namePlaceHolder=name)
+print '\n'
+
+parents, babies = (1, 1)
+while babies < 100:
+    print 'This generation has {0} babies'.format(babies)
+    parents, babies = (babies, parents + babies)
+print '\n'
+
+
+# Defining a function
+def greet(name):
+    print 'Hello', name
+greet('Jack')
+greet('Jill')
+greet('Bob')
+print '\n'
+
+
+for test_string in ['555-1212', 'ILL-EGAL']:
+    if re.match(r'^\d{3}-\d{4}$', test_string):
+        print test_string, 'is a valid US local phone number'
+    else:
+        print test_string, 'rejected'
+print '\n'
